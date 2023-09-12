594044963ee30dd55b4804c20d997dbd616020a1
Prabodh Meshram <prabodh.meshram7@gmail.com>
2023-09-12 15:42:06 +0530

    Add some final touches to footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,14 @@
+import { Typography } from "@material-tailwind/react";
 import React from "react";
 
 export default function Footer() {
   return (
     <div>
-      <footer>I am footer</footer>
+      <footer className="flex w-full flex-row items-center justify-center border-t bg-blue-gray-50 border-blue-gray-100 py-10 text-center">
+        <Typography color="blue-gray" className="font-normal">
+          &copy; 2023 Simple Todo Scheduler
+        </Typography>
+      </footer>
     </div>
   );
 }
